9448d3b34ae48e6c6c213629010d76e478170c67
Hugo Peixoto <hugo.peixoto@gmail.com>
2024-03-28 22:17:39 +0000

    First commit

---
+++ fetch-activity.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'json'
+require 'date'
+
+config = JSON.parse(File.read("config.json"))
+
+username = ARGV.fetch(0)
+
+def github_activity(username, token)
+  query = "query {
+    user(login: \"#{username}\") {
+      contributionsCollection {
+        contributionCalendar {
+          weeks {
+            contributionDays {
+              contributionCount
+            }
+          }
+        }
+      }
+    }
+  }"
+
+  URI("https://api.github.com/graphql")
+    .then { Net::HTTP.post(_1, JSON.dump({ query: }), { "Authorization" => "bearer #{token}" }) }
+    .then(&:body)
+    .then { JSON.parse(_1) }
+    .dig("data", "user", "contributionsCollection", "contributionCalendar", "weeks")
+    .flat_map { |w| w["contributionDays"] }
+    .map { |d| [d["date"], d["contributionCount"]] }
+    .to_h
+end
+
+def ansol_activity(username, token)
+  URI("https://git.ansol.org/api/v1/users/#{username}/heatmap")
+    .then { Net::HTTP.get(_1, { "Authorization" => "bearer #{token}" }) }
+    .then { JSON.parse(_1) }
+    .map { |x| [Time.at(x["timestamp"]).to_date.to_s, x["contributions"]] }
+    .group_by { |x| x[0] }
+    .transform_values { |v| v.map(&:last).sum }
+end
+
+def gitlab_activity(username)
+  URI("https://gitlab.com/users/#{username}/calendar.json")
+    .then { Net::HTTP.get(_1) }
+    .then { JSON.parse(_1) }
+end
+
+{
+  github: github_activity(username, config.dig("tokens", "github")),
+  ansol: ansol_activity(username, config.dig("tokens", "ansol")),
+  gitlab: gitlab_activity(username),
+}
+  .then { JSON.dump(_1) }
+  .then { File.write("activity.json", _1) }
---
+++ generate-html.rb
@@ -0,0 +1,96 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'date'
+
+activity = File
+  .read("activity.json")
+  .then { JSON.parse(_1) }
+
+finish = Date.parse(activity.values.flat_map(&:keys).max)
+start = finish.prev_year
+while not start.sunday?
+  start = start.prev_day
+end
+
+def bucket(contribs)
+  if contribs == 0
+    0
+  elsif contribs < 10
+    1
+  elsif contribs < 20
+    2
+  elsif contribs < 30
+    3
+  else
+    4
+  end
+end
+
+output = File.open("activity.html", "w+")
+
+output.puts <<EOF
+<html>
+  <style>
+    .calendar {
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      height: calc(10px * 7 + 3px * 6);
+      width: calc(10px * 53 + 3px * 52);
+      gap: 3px;
+      align-content: start;
+    }
+    .day {
+      width: 10px;
+      height: 10px;
+    }
+    .bucket0 { background-color: #ececef; }
+    .bucket1 { background-color: #d2dcff; }
+    .bucket2 { background-color: #7992f5; }
+    .bucket3 { background-color: #3f51ae; }
+    .bucket4 { background-color: #2a2b59; }
+    h2 { font-family: monospace; text-align: center; }
+    body {
+      padding-top: 20px;
+      margin: 0px auto;
+      width: calc(10px * 53 + 3px * 52);
+    }
+  </style>
+
+  <body>
+EOF
+
+def graph(label, range, &fn)
+  s = ""
+  s << "<h2>#{label}</h2>\n"
+  s << "<div class='calendar'>\n"
+  range.each do |day|
+    s << "<div class='day bucket#{bucket(fn[day])}'></div>"
+  end
+  s << "\n</div>\n"
+  s
+end
+
+output << graph("activity", start..finish) do |day|
+  activity["ansol"].fetch(day.to_s, 0) +
+  activity["gitlab"].fetch(day.to_s, 0) +
+  activity["github"].fetch(day.to_s, 0)
+end
+
+output << graph("git.ansol.org", start..finish) do |day|
+  activity["ansol"].fetch(day.to_s, 0)
+end
+
+output << graph("gitlab.com", start..finish) do |day|
+  activity["gitlab"].fetch(day.to_s, 0)
+end
+
+output << graph("github.com", start..finish) do |day|
+  activity["github"].fetch(day.to_s, 0)
+end
+
+output.puts <<EOF
+  </body>
+</html>
+EOF
